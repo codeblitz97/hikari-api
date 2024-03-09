cb8728361e9d6bc8e98f29223be967a69b9cd529
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-03-09 15:44:56 +0600

    Change puppeteer path

--- src/char.ts
+++ src/char.ts
@@ -2,6 +2,11 @@ import CharacterAI from 'node_characterai';
 
 const characterAI = new CharacterAI();
 
+characterAI.requester.puppeteerPath =
+  process.env.NODE_ENV === 'production'
+    ? (process.env.PUPPETEER_EXECUTABLE_PATH as string)
+    : characterAI.requester.puppeteerPath;
+
 export async function authenticateCharacter() {
   await characterAI.authenticateWithToken(process.env.SESSION_TOKEN as string);
 }
